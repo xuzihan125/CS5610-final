b8d0292dce88a95b106bffd736af9b1ae8829ba3
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-12-12 12:57:41 -0500

    Added user following user

--- src/Project/Users/details.js
+++ src/Project/Users/details.js
@@ -1,19 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import * as client from './client';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { current } from '@reduxjs/toolkit';
+import * as client from './client.js';
+import * as followsClient from '../Follows/client.js';
 
 function UserDetail() {
 
     const [user, setUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState(null);
+    const [followers, setFollowers] = useState([]);
+    const [following, setFollowing] = useState([]);
     const { userId } = useParams();
-    const currentUser = useSelector((state) => state.currentUser);
+    // const currentUser = useSelector((state) => state.currentUser);
+
+    const navigate = useNavigate();
+
+    const updateUser = async () => {
+        const status = await client.updateUser(userId, user);
+    }
+
+    const fetchCurrentUser = async () => {
+        const cu = await client.account();
+        setCurrentUser(cu);
+    }
+
+    const deleteUser = async () => {
+        const status = await client.deleteUser(userId);
+        navigate('/users');
+    }
+
+    const followUser = async () => {
+        const status = await followsClient.userFollowsUser(userId);
+        fetchFollowing();
+    }
+
+    const unfollowUser = async () => {
+        const status = await followsClient.userUnfollowsUser(userId);
+        fetchFollowing();
+    }
+
+    const fetchFollowers = async () => {
+        const f = await followsClient.findFollowersOfUser(userId);
+        setFollowers(f);
+    }
+
+    const fetchFollowing = async () => {
+        const f = await followsClient.findFollowingUsersByUser(userId);
+        setFollowing(f);
+    }
+
+    const alreadyFollowing = () => {
+        return followers.some((follows) => {
+            return follows.follower._id === currentUser._id;
+        })
+    }
 
     const fetchUser = async () => {
         try {
             const u = await client.findUserById(userId);
             setUser(u);
+            console.log(typeof u._id, u._id);
+            console.log(typeof currentUser._id, currentUser._id);
         } catch (error) {
             console.error(error);
         }
@@ -21,16 +68,81 @@ function UserDetail() {
 
     useEffect(() => {
         fetchUser();
-    }, [userId])
+        fetchCurrentUser();
+        fetchFollowers();
+        fetchFollowing();
+    }, [userId, following])
 
     return (
         <div>
+            {currentUser && currentUser._id !== userId && (
+                <>
+                    {alreadyFollowing() ? (
+                        <button onClick={unfollowUser} className="btn btn-danger float-end">
+                            Unfollow
+                        </button>
+                    ) : (
+                        <button onClick={followUser} className="btn btn-warning float-end">
+                            Follow
+                        </button>
+                    )}
+                </>
+            )}
+            <h2>User Details</h2>
             {user && (
-                <h1>{user.username}</h1>
-
+                <div>
+                    <p>Username: {user.username}</p>
+                    <p>Email: {user.email}</p>
+                    <p>
+                        First Name:
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={user.firstName}
+                            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+                        />
+                    </p>
+                    <p>Last Name: {user.lastName}</p>
+                    <button onClick={updateUser} className="btn btn-primary">
+                        Update
+                    </button>
+                    <button
+                        onClick={() => deleteUser(user._id)}
+                        className="btn btn-danger"
+                    >
+                        Delete
+                    </button>
+                    <h3>Followers</h3>
+                    <div className="list-group">
+                        {followers.map((follows, index) => (
+                            <Link
+                                key={index}
+                                className="list-group-item"
+                                to={`/project/users/${follows.follower._id}`}
+                            >
+                                {follows.follower.username}
+                                {follows.follower._id}
+                            </Link>
+                        ))}
+                    </div>
+                    <h3>Following</h3>
+                    <div className="list-group">
+                        {following.map((follows, index) => (
+                            <Link
+                                key={index}
+                                className="list-group-item"
+                                to={`/project/users/${follows.followed._id}`}
+                            >
+                                {follows.followed.username}
+                                {follows.followed._id}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
             )}
         </div>
     )
 }
+
 
 export default UserDetail;
