2c539b82eafae25de26592a8c3291eb3b148635a
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-12-14 06:10:53 -0500

    Fixed line break issue

--- src/Project/Recipes/Components/RecipeInstructions.js
+++ src/Project/Recipes/Components/RecipeInstructions.js
@@ -1,15 +1,33 @@
 import React from "react";
 
 function RecipeInstructions({ instructions }) {
+
+    const renderInstructions = (instructions) => {
+        if (instructions) {
+            if (instructions.includes('<br />')) {
+                return (
+                    <div>{instructions.split("<br />").map((line, index) => (
+                        <React.Fragment key={index}>
+                            {line}
+                            {index !== instructions.split("<br />").length - 1 && <br />}
+                        </React.Fragment>
+                    ))}</div>
+                )
+            } else {
+                return (
+                    <div>{instructions}</div>
+                )
+            }
+        } else {
+            return (
+                <div>No instructions available</div>
+            )
+        }
+    }
     return (
         <div>
-            <h3>Steps:</h3>
-            <div>{instructions.split("<br />").map((line, index) => (
-                <React.Fragment key={index}>
-                    {line}
-                    {index !== instructions.split("<br />").length - 1 && <br />}
-                </React.Fragment>
-            )) || "No instructions available"}</div>
+            <h3>Instructions: </h3>
+            {renderInstructions(instructions)}
         </div>
     )
 }
